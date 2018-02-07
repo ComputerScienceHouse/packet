3f7dea94b98d817476acab89430343e5ef4e8727
Marc Billow <mbillow@me.com>
2018-02-07 06:12:23 -0500

    Add Flask-Restless and Initial API routes

--- packet/__init__.py
+++ packet/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restless import APIManager
 
 app = Flask(__name__)
 
@@ -11,11 +12,15 @@ if os.path.exists(os.path.join(os.getcwd(), "config.py")):
 else:
     app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
 
-
+# Create the database session and import models.
 db = SQLAlchemy(app)
 from packet.models import *
 migrate = Migrate(app, db)
 
+# Initialize the Restless API manager
+manager = APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(Freshman, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Packet, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 @app.route('/')
 def hello_world():
