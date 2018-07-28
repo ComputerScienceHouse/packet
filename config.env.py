796bf8e5c206fcb27401bdcc772742202cebcba5
Joel Eager <jee7291@rit.edu>
2018-07-28 17:09:51 -0400

    Improved config logic

--- config.env.py
+++ config.env.py
@@ -9,11 +9,8 @@ SECRET_KEY = environ.get("PACKET_SECRET_KEY", "PLEASE_REPLACE_ME")
 
 # OpenID Connect SSO config
 OIDC_ISSUER = environ.get("PACKET_OIDC_ISSUER", "https://sso.csh.rit.edu/auth/realms/csh")
-OIDC_CLIENT_CONFIG = {
-    "client_id": environ.get("PACKET_OIDC_CLIENT_ID", "packet"),
-    "client_secret": environ.get("PACKET_OIDC_CLIENT_SECRET", "PLEASE_REPLACE_ME"),
-    "post_logout_redirect_uris": [environ.get("PACKET_OIDC_LOGOUT_REDIRECT_URI", "https://" + SERVER_NAME + "/logout")]
-}
+OIDC_CLIENT_ID = environ.get("PACKET_OIDC_CLIENT_ID", "packet")
+OIDC_CLIENT_SECRET = environ.get("PACKET_OIDC_CLIENT_SECRET", "PLEASE_REPLACE_ME")
 
 # SQLAlchemy config
 SQLALCHEMY_DATABASE_URI = environ.get("PACKET_DATABASE_URI", None)
--- packet/__init__.py
+++ packet/__init__.py
@@ -10,16 +10,22 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+# Load default configuration and any environment variable overrides
+app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+
+# Load file based configuration overrides if present
 if os.path.exists(os.path.join(os.getcwd(), "config.py")):
     app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
-else:
-    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
 
+# Initialize the extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"],
-                          client_registration_info=app.config["OIDC_CLIENT_CONFIG"])
+auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"], client_registration_info={
+    "client_id": app.config["OIDC_CLIENT_ID"],
+    "client_secret": app.config["OIDC_CLIENT_SECRET"]
+})
 
 # pylint: disable=wrong-import-position
 from . import routes
+from . import models
