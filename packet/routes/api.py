1d3597a7d7783f9269916ff241bd6e8f1ddd3243
zedoax <zedoax@anagrio.net>
2018-08-30 16:12:41 -0400

    Fixed AttributeError

--- packet/routes/api.py
+++ packet/routes/api.py
@@ -7,8 +7,12 @@ from packet.packet import sign
 @auth.oidc_auth
 @before_request
 def sign(member_username, packet_username, info):
-    if info.uid != member_username and "eboard-evaluations" not in info.member_info.group_list:
-        return "Error: UID Submission Mismatch"
+    if info.uid != member_username:
+        if info.member_info:
+            if "eboard-evaluations" not in info.member_info.group_list:
+                return "Error: You are not evals"
+        else:
+            return "Error: UID Submission Mismatch"
     if not sign(member_username, packet_username):
         return "Error: Signature not valid.  Reason: Unknown"
     return "Success: Signed Packet: " + packet_username
