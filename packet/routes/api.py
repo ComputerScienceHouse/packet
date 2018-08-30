ffa8062a43769bdf5660a3d9c1d708a70574e501
zedoax <zedoax@anagrio.net>
2018-08-30 15:15:21 -0400

    Added eval support to signing route

--- packet/routes/api.py
+++ packet/routes/api.py
@@ -6,8 +6,8 @@ from packet.packet import sign
 @app.route("/api/v1/<member_username>/sign/<packet_username>")
 @auth.oidc_auth
 @before_request
-def sign(member_username, packet_username, uid):
-    if uid != member_username:
+def sign(member_username, packet_username, info):
+    if info.uid != member_username and "eboard-evaluations" not in info.member_info.group_list:
         return "Error: UID Submission Mismatch"
     if not sign(member_username, packet_username):
         return "Error: Signature not valid.  Reason: Unknown"
