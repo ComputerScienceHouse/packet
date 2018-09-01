1ae99e9f551cf3becc98cc9c85bd91dfaf43c960
zedoax <zedoax@anagrio.net>
2018-09-01 17:32:33 -0400

    Changes queries to simply return the list of signatures, not a query object

--- packet/member.py
+++ packet/member.py
@@ -4,8 +4,8 @@ from .models import Freshman, FreshSignature, UpperSignature, MiscSignature
 def signed_packets(member):
     is_freshman = Freshman.query.filter_by(rit_username=member).first() is not None
     if is_freshman:
-        return FreshSignature.query.filter_by(freshman_username=member, signed=True)
+        return FreshSignature.query.filter_by(freshman_username=member, signed=True).all()
     is_upper = UpperSignature.query.filter_by(member=member).first() is not None
     if is_upper:
-        return UpperSignature.query.filter_by(member=member, signed=True)
-    return MiscSignature.query.filter_by(member=member)
+        return UpperSignature.query.filter_by(member=member, signed=True).all()
+    return MiscSignature.query.filter_by(member=member).all()
