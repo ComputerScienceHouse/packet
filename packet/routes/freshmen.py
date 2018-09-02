bbcd1c460b721f30d1865a55339a54f1779efc0a
Devin Matte <devinmatte@gmail.com>
2018-09-02 01:33:12 -0400

    Essays for freshmen

--- packet/routes/freshmen.py
+++ packet/routes/freshmen.py
@@ -1,4 +1,4 @@
-from flask import redirect
+from flask import redirect, render_template
 
 from packet import auth, app
 from packet.utils import before_request
@@ -9,3 +9,10 @@ from packet.utils import before_request
 @before_request
 def index(info=None):
     return redirect("/packet/" + info['uid'], 302)
+
+
+@app.route("/essays")
+@auth.oidc_auth
+@before_request
+def essays(info=None):
+    return render_template("essays.html", info=info)
