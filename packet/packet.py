8d117d8a95b068a180b98461f1281add271cdeb3
Devin Matte <devinmatte@gmail.com>
2018-09-02 21:21:49 -0400

    Cleaning up check orders

--- packet/packet.py
+++ packet/packet.py
@@ -10,16 +10,8 @@ def sign(signer_username, freshman_username):
     if freshman_signed is None:
         return False
     packet = freshman_signed.current_packet()
-    if packet is None:
+    if packet is None or not packet.is_open():
         return False
-    if not packet.is_open():
-        return False
-
-    # Make sure only on floor freshmen can sign packets
-    freshman_signer = Freshman.query.filter_by(rit_username=signer_username).first()
-    if freshman_signer:
-        if not freshman_signer.onfloor:
-            return False
 
     upper_signature = UpperSignature.query.filter(UpperSignature.member == signer_username,
                                                   UpperSignature.packet == packet).first()
@@ -31,6 +23,11 @@ def sign(signer_username, freshman_username):
             return False
         upper_signature.signed = True
     elif fresh_signature:
+        # Make sure only on floor freshmen can sign packets
+        freshman_signer = Freshman.query.filter_by(rit_username=signer_username).first()
+        if freshman_signer:
+            if not freshman_signer.onfloor:
+                return False
         fresh_signature.signed = True
     else:
         db.session.add(MiscSignature(packet=packet, member=signer_username))
