75cf8b2d8f634a34861fe57f0cb2068475e20fe9
Devin Matte <devinmatte@gmail.com>
2018-09-04 11:02:21 -0400

    Showing data for on and off floor upperclassmen

--- packet/routes/upperclassmen.py
+++ packet/routes/upperclassmen.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from itertools import chain
+
 from flask import redirect, render_template
 
 from packet import auth, app
-from packet.models import UpperSignature
+from packet.models import Packet
 from packet.utils import before_request
 
 
@@ -15,5 +18,16 @@ def index():
 @auth.oidc_auth
 @before_request
 def upperclassman(uid, info=None):
-    signatures = UpperSignature.query.filter_by(member=uid).order_by(UpperSignature.signed.desc())
-    return render_template("upperclassman.html", info=info, signatures=signatures, member=uid)
+    open_packets = Packet.query.filter(Packet.end > datetime.now()).filter(Packet.start < datetime.now()).all()
+
+    for packet in open_packets:
+        packet.did_sign = False
+
+        for sig in chain(filter(lambda sig: sig.signed, packet.upper_signatures), packet.misc_signatures):
+            if sig.member == uid:
+                packet.did_sign = True
+                break
+
+    open_packets.sort(key=lambda x: x.did_sign, reverse=True)
+
+    return render_template("upperclassman.html", info=info, open_packets=open_packets, member=uid)
