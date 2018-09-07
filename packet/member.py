5a9699176e25be5cf7f10e9809210a4132520ea5
zedoax <zedoax@anagrio.net>
2018-09-07 15:48:28 -0400

    Apparently the merge undid some things

--- packet/member.py
+++ packet/member.py
@@ -3,17 +3,15 @@ from datetime import datetime
 from .models import Packet, Freshman, FreshSignature, UpperSignature, MiscSignature, db
 
 
-def signed_packets(member):
+def signed_packets(member, intro=False):
     # Checks whether or not member is a freshman
-    if db.session.query(FreshSignature.freshman_username) \
-            .filter(FreshSignature.freshman_username == member).count() > 0:
+    if intro:
         return db.session.query(Packet.freshman_username, Freshman.name, FreshSignature.signed) \
             .select_from(FreshSignature).join(Packet).join(Freshman) \
             .filter(FreshSignature.freshman_username == member) \
             .filter(Packet.end > datetime.now()).filter(Packet.start < datetime.now()).all()
     # Checks whether or not member is an upperclassman
-    if db.session.query(UpperSignature.member).join(Packet).join(Freshman) \
-            .filter(UpperSignature.member == member).count() > 0:
+    if db.session.query(UpperSignature.member).filter(UpperSignature.member == member).count() > 0:
         return db.session.query(Freshman.rit_username, Freshman.name, UpperSignature.signed) \
             .select_from(UpperSignature).join(Packet).join(Freshman) \
             .filter(UpperSignature.member == member) \
