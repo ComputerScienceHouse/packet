6b070a698e8d1d125db24aa22d4d5421eb15aca8
Joel Eager <jee7291@rit.edu>
2018-09-14 03:09:10 -0400

    Refactored and fixed packet signing

--- packet/routes/api.py
+++ packet/routes/api.py
@@ -1,19 +1,31 @@
-from packet import auth, app
+from packet import auth, app, db
 from packet.utils import before_request
-from packet.packet import sign as sign_packet
+from packet.models import Packet, MiscSignature
 
 
-@app.route("/api/v1/<member_username>/sign/<packet_username>", methods=["POST"])
+@app.route("/api/v1/sign/<packet_username>/<packet_id>/", methods=["POST"])
 @auth.oidc_auth
 @before_request
-def sign(member_username, packet_username, info):
-    if info['uid'] != member_username:
-        if info['member_info']:
-            if "eboard-evaluations" not in info['member_info']['group_list']:
-                return "Error: You are not evals"
-        else:
-            return "Error: UID Submission Mismatch"
-    if not sign_packet(member_username, packet_username):
-        return "Error: Signature not valid.  Reason: Unknown"
-    return "Success: Signed Packet: " + packet_username
+def sign(packet_username, packet_id, info):
+    packet = Packet.query.filter_by(freshman_username=packet_username, id=packet_id).first()
 
+    if packet is not None and packet.is_open():
+        if app.config["REALM"] == "csh":
+            # Check if the CSHer is an upperclassman and if so, sign that row
+            for sig in filter(lambda sig: sig.member == info["uid"], packet.upper_signatures):
+                sig.signed = True
+                db.session.commit()
+                return "Success: Signed Packet: " + packet_username
+
+            # The CSHer is a misc so add a new row
+            db.session.add(MiscSignature(packet=packet, member=info["uid"]))
+            db.session.commit()
+            return "Success: Signed Packet: " + packet_username
+        else:
+            # Check if the freshman is onfloor and if so, sign that row
+            for sig in filter(lambda sig: sig.freshman_username == info["uid"], packet.fresh_signatures):
+                sig.signed = True
+                db.session.commit()
+                return "Success: Signed Packet: " + packet_username
+
+    return "Error: Signature not valid.  Reason: Unknown"
