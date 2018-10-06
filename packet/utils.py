c5d157b352b0116d427849bc7dc404f0bded4a42
Joel Eager <jee7291@rit.edu>
2018-10-06 18:16:16 -0400

    Cleaned up the before_request decorator

--- packet/utils.py
+++ packet/utils.py
@@ -1,23 +1,19 @@
-# Credit to Liam Middlebrook and Ram Zallan
-# https://github.com/liam-middlebrook/gallery
 from functools import wraps, lru_cache
 
 import requests
 from flask import session
 
-from packet import _ldap, auth, app
+from packet import auth, app
 from packet.models import Freshman
-from packet.ldap import (ldap_get_member,
-                         ldap_is_active,
-                         ldap_is_onfloor,
-                         ldap_get_roomnumber,
-                         ldap_get_groups,
-                         ldap_is_intromember)
+from packet.ldap import ldap_get_member, ldap_is_intromember
 
 INTRO_REALM = "https://sso.csh.rit.edu/auth/realms/intro"
 
-
 def before_request(func):
+    """
+    Credit to Liam Middlebrook and Ram Zallan
+    https://github.com/liam-middlebrook/gallery
+    """
     @wraps(func)
     def wrapped_function(*args, **kwargs):
         uid = str(session["userinfo"].get("preferred_username", ""))
@@ -29,15 +25,9 @@ def before_request(func):
                 "onfloor": is_on_floor(uid)
             }
         else:
-            uuid = str(session["userinfo"].get("sub", ""))
-            user_obj = _ldap.get_member(uid, uid=True)
             info = {
                 "realm": "csh",
-                "uuid": uuid,
-                "uid": uid,
-                "user_obj": user_obj,
-                "member_info": get_member_info(uid),
-                "color": requests.get('https://themeswitcher.csh.rit.edu/api/colour').content
+                "uid": uid
             }
 
         kwargs["info"] = info
@@ -46,25 +36,12 @@ def before_request(func):
     return wrapped_function
 
 
-@lru_cache(maxsize=2048)
-def get_member_info(uid):
-    account = ldap_get_member(uid)
-
-    member_info = {
-        "user_obj": account,
-        "group_list": ldap_get_groups(account),
-        "uid": account.uid,
-        "name": account.cn,
-        "active": ldap_is_active(account),
-        "onfloor": ldap_is_onfloor(account),
-        "room": ldap_get_roomnumber(account),
-    }
-    return member_info
-
-
-@lru_cache(maxsize=2048)
-def is_on_floor(uid):
-    freshman = Freshman.query.filter_by(rit_username=uid).first()
+@lru_cache(maxsize=128)
+def is_on_floor(rit_username):
+    """
+    Checks if a freshman is on floor
+    """
+    freshman = Freshman.query.filter_by(rit_username=rit_username).first()
     if freshman is not None:
         return freshman.onfloor
     else:
@@ -85,6 +62,7 @@ def packet_auth(func):
         return func(*args, **kwargs)
 
     return wrapped_function
+
 
 def notify_slack(name: str):
     """
