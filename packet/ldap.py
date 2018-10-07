69c250c14f74d257d124f5c4abcb27b08fe7cd04
Joel Eager <jee7291@rit.edu>
2018-10-07 14:30:28 -0400

    Cleaned out unused functions and overly aggressive caching from ldap.py

--- packet/ldap.py
+++ packet/ldap.py
@@ -3,7 +3,6 @@ from functools import lru_cache
 from packet import _ldap
 
 
-@lru_cache(maxsize=1024)
 def _ldap_get_group_members(group):
     return _ldap.get_group(group).get_members()
 
@@ -16,48 +15,21 @@ def _ldap_is_member_of_group(member, group):
     return False
 
 
-@lru_cache(maxsize=1024)
-def _ldap_is_member_of_directorship(account, directorship):
-    directors = _ldap.get_directorship_heads(directorship)
-    for director in directors:
-        if director.uid == account.uid:
-            return True
-    return False
-
-
 # Getters
 
-@lru_cache(maxsize=4096)
+@lru_cache(maxsize=256)
 def ldap_get_member(username):
     return _ldap.get_member(username, uid=True)
 
 
-@lru_cache(maxsize=1024)
 def ldap_get_active_members():
     return _ldap_get_group_members("active")
 
 
-@lru_cache(maxsize=1024)
 def ldap_get_intro_members():
     return _ldap_get_group_members("intromembers")
 
 
-@lru_cache(maxsize=1024)
-def ldap_get_onfloor_members():
-    return _ldap_get_group_members("onfloor")
-
-
-@lru_cache(maxsize=1024)
-def ldap_get_groups(account):
-    group_list = account.get("memberOf")
-    groups = []
-    for group_dn in group_list:
-        if "cn=groups,cn=accounts" in group_dn:
-            groups.append(group_dn.split(",")[0][3:])
-    return groups
-
-
-@lru_cache(maxsize=1024)
 def ldap_get_eboard():
     members = _ldap_get_group_members("eboard-chairman") + _ldap_get_group_members("eboard-evaluations"
         ) + _ldap_get_group_members("eboard-financial") + _ldap_get_group_members("eboard-history"
@@ -68,63 +40,28 @@ def ldap_get_eboard():
     return members
 
 
-@lru_cache(maxsize=2048)
 def ldap_get_live_onfloor():
     """
     :return: All upperclassmen who live on floor and are not eboard
     """
     members = []
-    onfloor = ldap_get_onfloor_members()
+    onfloor = _ldap_get_group_members("onfloor")
     for member in onfloor:
         if ldap_get_roomnumber(member) and not ldap_is_eboard(member):
             members.append(member)
+
     return members
 
 
 # Status checkers
 
-@lru_cache(maxsize=1024)
-def ldap_is_eval_director(account):
-    return _ldap_is_member_of_directorship(account, 'evaluations')
-
-
-@lru_cache(maxsize=1024)
-def ldap_is_active(account):
-    return _ldap_is_member_of_group(account, 'active')
-
-
-@lru_cache(maxsize=1024)
-def ldap_is_alumni(account):
-    # If the user is not active, they are an alumni.
-    return not _ldap_is_member_of_group(account, 'active')
-
-
-@lru_cache(maxsize=1024)
 def ldap_is_eboard(account):
     return _ldap_is_member_of_group(account, 'eboard')
 
 
-@lru_cache(maxsize=1024)
-def ldap_is_rtp(account):
-    return _ldap_is_member_of_group(account, 'rtp')
-
-
-@lru_cache(maxsize=1024)
 def ldap_is_intromember(account):
     return _ldap_is_member_of_group(account, 'intromembers')
 
-
-@lru_cache(maxsize=1024)
-def ldap_is_onfloor(account):
-    return _ldap_is_member_of_group(account, 'onfloor')
-
-
-@lru_cache(maxsize=1024)
-def ldap_is_current_student(account):
-    return _ldap_is_member_of_group(account, 'current_student')
-
-
-@lru_cache(maxsize=1024)
 def ldap_get_roomnumber(account):
     try:
         return account.roomNumber
