438630acce0538c3f9c8d01eaf85d083dac35834
Joel Eager <jee7291@rit.edu>
2018-10-13 13:32:46 -0400

    Improved version configuration and on-load logging

--- packet/__init__.py
+++ packet/__init__.py
@@ -4,6 +4,7 @@ The application setup and initialization code lives here.
 
 import os
 import logging
+import json
 
 import csh_ldap
 from flask import Flask
@@ -24,14 +25,18 @@ _pyfile_config = os.path.join(_root_dir, "config.py")
 if os.path.exists(_pyfile_config):
     app.config.from_pyfile(_pyfile_config)
 
-app.config["VERSION"] = __version__
+# Fetch the version number from the npm package file
+_package_file = open(os.path.join(_root_dir, "package.json"))
+app.config["VERSION"] = json.load(_package_file)["version"]
 
 # Logger configuration
 logging.getLogger().setLevel(app.config["LOG_LEVEL"])
+app.logger.info("Launching packet v" + app.config["VERSION"])
 
 # Initialize the extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+app.logger.info("SQLAlchemy pointed at " + repr(db.engine.url))
 
 auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"], client_registration_info={
     "client_id": app.config["OIDC_CLIENT_ID"],
@@ -42,7 +47,7 @@ auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"], client_registra
 # LDAP
 _ldap = csh_ldap.CSHLDAP(app.config["LDAP_BIND_DN"], app.config["LDAP_BIND_PASS"])
 
-app.logger.info("DB and LDAP configured")
+app.logger.info("OIDCAuth and LDAP configured")
 
 # pylint: disable=wrong-import-position
 from . import models
