fbb54824a2ceeb8a299433ab77d35a387cb46230
Devin Matte <devinmatte@gmail.com>
2019-08-15 22:10:43 -0400

    Completed Notifications

--- packet/notifications.py
+++ packet/notifications.py
@@ -1,12 +1,14 @@
 import onesignal
-from . import app, onesignal_client
+
+from packet import app, onesignal_client
+from packet.models import NotificationSubscription
 
 post_body = {
     "content": {"en": "Default message"},
     "headings": {"en": "Default Title"},
     "included_segments": ["Active Users", "Inactive Users"],
-    "chrome_web_icon": app.config["PROTOCOL"] + app.config["SERVER_NAME"]  + "/static/android-chrome-512x512.png",
-    "chrome_web_badge": app.config["PROTOCOL"] + app.config["SERVER_NAME"]  + "/static/android-chrome-512x512.png",
+    "chrome_web_icon": app.config["PROTOCOL"] + app.config["SERVER_NAME"] + "/static/android-chrome-512x512.png",
+    "chrome_web_badge": app.config["PROTOCOL"] + app.config["SERVER_NAME"] + "/static/android-chrome-512x512.png",
     "url": app.config["PROTOCOL"] + app.config["SERVER_NAME"]
 }
 
@@ -40,7 +42,7 @@ def packet_100_percent_notification(packet):
         # TODO: Issue #156
         notification.post_body["chrome_web_icon"] = 'https://profiles.csh.rit.edu/image/' + packet.freshman_username
         notification.post_body["include_player_ids"] = tokens
-        
+
         onesignal_response = onesignal_client.send_notification(notification)
         print(onesignal_response.status_code)
         print(onesignal_response.json())
