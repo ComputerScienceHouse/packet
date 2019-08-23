b689a706e990b8a96b6f5a53276d822ae4e27103
Devin Matte <devinmatte@gmail.com>
2019-08-23 19:17:55 -0400

    Fixing quote inconsistencies

--- packet/mail.py
+++ packet/mail.py
@@ -7,31 +7,31 @@ mail = Mail(app)
 
 
 def send_start_packet_mail(packet):
-    if app.config['MAIL_PROD']:
+    if app.config["MAIL_PROD"]:
         recipients = ["<" + packet.freshman.rit_username + "@rit.edu>"]
-        msg = Message(subject="CSH Packet Starts " + packet.start.strftime('%A, %B %-d'),
+        msg = Message(subject="CSH Packet Starts " + packet.start.strftime("%A, %B %-d"),
                       sender=app.config.get("MAIL_USERNAME"),
                       recipients=recipients)
 
-        template = 'mail/packet_start'
-        msg.body = render_template(template + '.txt', packet=packet)
-        msg.html = render_template(template + '.html', packet=packet)
+        template = "mail/packet_start"
+        msg.body = render_template(template + ".txt", packet=packet)
+        msg.html = render_template(template + ".html", packet=packet)
         app.logger.info("Sending mail to " + recipients[0])
         mail.send(msg)
 
 
 def send_report_mail(form_results, reporter):
-    if app.config['MAIL_PROD']:
+    if app.config["MAIL_PROD"]:
         recipients = ["<evals@csh.rit.edu>"]
         msg = Message(subject="Packet Report",
                       sender=app.config.get("MAIL_USERNAME"),
                       recipients=recipients)
 
-        person = form_results['person']
-        report = form_results['report']
+        person = form_results["person"]
+        report = form_results["report"]
 
-        template = 'mail/report'
-        msg.body = render_template(template + '.txt', person=person, report=report, reporter=reporter)
-        msg.html = render_template(template + '.html', person=person, report=report, reporter=reporter)
+        template = "mail/report"
+        msg.body = render_template(template + ".txt", person=person, report=report, reporter=reporter)
+        msg.html = render_template(template + ".html", person=person, report=report, reporter=reporter)
         app.logger.info("Sending mail to " + recipients[0])
         mail.send(msg)
--- packet/notifications.py
+++ packet/notifications.py
@@ -28,9 +28,9 @@ def packet_signed_notification(packet, signer):
     subscriptions = NotificationSubscription.query.filter_by(freshman_username=packet.freshman_username)
     if subscriptions:
         notification_body = post_body
-        notification_body["contents"]["en"] = signer + ' signed your packet! Congrats or I\'m Sorry'
-        notification_body["headings"]["en"] = 'New Packet Signature!'
-        notification_body["chrome_web_icon"] = 'https://profiles.csh.rit.edu/image/' + signer
+        notification_body["contents"]["en"] = signer + " signed your packet! Congrats or I'm Sorry"
+        notification_body["headings"]["en"] = "New Packet Signature!"
+        notification_body["chrome_web_icon"] = "https://profiles.csh.rit.edu/image/" + signer
         notification_body["url"] = app.config["PROTOCOL"] + app.config["PACKET_INTRO"]
 
         send_notification(notification_body, subscriptions, intro_onesignal_client)
@@ -41,10 +41,10 @@ def packet_100_percent_notification(packet):
     intro_subscriptions = NotificationSubscription.query.filter(NotificationSubscription.freshman_username.isnot(None))
     if member_subscriptions or intro_subscriptions:
         notification_body = post_body
-        notification_body["contents"]["en"] = packet.freshman.name + ' got 💯 on packet!'
-        notification_body["headings"]["en"] = 'New 100% on Packet!'
+        notification_body["contents"]["en"] = packet.freshman.name + " got 💯 on packet!"
+        notification_body["headings"]["en"] = "New 100% on Packet!"
         # TODO: Issue #156
-        notification_body["chrome_web_icon"] = 'https://profiles.csh.rit.edu/image/' + packet.freshman_username
+        notification_body["chrome_web_icon"] = "https://profiles.csh.rit.edu/image/" + packet.freshman_username
 
         send_notification(notification_body, member_subscriptions, csh_onesignal_client)
         send_notification(notification_body, intro_subscriptions, intro_onesignal_client)
@@ -54,8 +54,8 @@ def packet_starting_notification(packet):
     subscriptions = NotificationSubscription.query.filter_by(freshman_username=packet.freshman_username)
     if subscriptions:
         notification_body = post_body
-        notification_body["contents"]["en"] = 'Log into your packet, and get started meeting people!'
-        notification_body["headings"]["en"] = 'Your packet has begun!'
+        notification_body["contents"]["en"] = "Log into your packet, and get started meeting people!"
+        notification_body["headings"]["en"] = "Your packet has begun!"
         notification_body["url"] = app.config["PROTOCOL"] + app.config["PACKET_INTRO"]
         notification_body["send_after"] = packet.start.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -66,8 +66,8 @@ def packets_starting_notification(start_date):
     member_subscriptions = NotificationSubscription.query.filter(NotificationSubscription.member.isnot(None))
     if member_subscriptions:
         notification_body = post_body
-        notification_body["contents"]["en"] = 'New packets have started, visit packet to see them!'
-        notification_body["headings"]["en"] = 'Packets Start Today!'
+        notification_body["contents"]["en"] = "New packets have started, visit packet to see them!"
+        notification_body["headings"]["en"] = "Packets Start Today!"
         notification_body["send_after"] = start_date.strftime("%Y-%m-%d %H:%M:%S")
 
         send_notification(notification_body, member_subscriptions, csh_onesignal_client)
