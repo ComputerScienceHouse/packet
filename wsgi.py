7164a5c53e815e226c6ef1b9e29ac0b17e8ac21b
Devin Matte <devinmatte@gmail.com>
2019-09-22 21:25:29 -0400

    Adds back threading to wsgi

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ Primary entry point for the app
 from packet import app
 
 if __name__ == "__main__":
-    app.run(host=app.config["IP"], port=int(app.config["PORT"]), threaded=False)
+    app.run(host=app.config["IP"], port=int(app.config["PORT"]))
