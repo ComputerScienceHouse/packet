6c94112136417a286d5bc92ebc24c46ea2bd897f
Devin Matte <devinmatte@gmail.com>
2020-08-19 10:22:29 -0400

    Fixing for frosh

--- packet/utils.py
+++ packet/utils.py
@@ -13,23 +13,25 @@ from packet.ldap import ldap_get_member, ldap_is_intromember, ldap_is_evals, lda
 
 INTRO_REALM = 'https://sso.csh.rit.edu/auth/realms/intro'
 
+
 def before_request(func):
     """
     Credit to Liam Middlebrook and Ram Zallan
     https://github.com/liam-middlebrook/gallery
     """
+
     @wraps(func)
     def wrapped_function(*args, **kwargs):
         uid = str(session['userinfo'].get('preferred_username', ''))
-        member = ldap_get_member(uid)
-
         if session['id_token']['iss'] == INTRO_REALM:
             info = {
                 'realm': 'intro',
                 'uid': uid,
-                'onfloor': is_freshman_on_floor(uid)
+                'onfloor': is_freshman_on_floor(uid),
+                'admin': False  # It's always false if frosh
             }
         else:
+            member = ldap_get_member(uid)
             info = {
                 'realm': 'csh',
                 'uid': uid,
@@ -58,6 +60,7 @@ def packet_auth(func):
     """
     Decorator for easily configuring oidc
     """
+
     @auth.oidc_auth('app')
     @wraps(func)
     def wrapped_function(*args, **kwargs):
@@ -76,6 +79,7 @@ def admin_auth(func):
     """
     Decorator for easily configuring oidc
     """
+
     @auth.oidc_auth('app')
     @wraps(func)
     def wrapped_function(*args, **kwargs):
@@ -85,6 +89,8 @@ def admin_auth(func):
             if not ldap_is_evals(member) and not ldap_is_rtp(member):
                 app.logger.warn('Stopped member {} from accessing admin UI'.format(username))
                 return redirect(app.config['PROTOCOL'] + app.config['PACKET_UPPER'], code=301)
+        else:
+            return redirect(app.config['PROTOCOL'] + app.config['PACKET_INTRO'], code=301)
 
         return func(*args, **kwargs)
 
@@ -100,7 +106,7 @@ def notify_slack(name: str):
         return
 
     msg = f':pizza-party: {name} got :100: on packet! :pizza-party:'
-    requests.put(app.config['SLACK_WEBHOOK_URL'], json={'text':msg})
+    requests.put(app.config['SLACK_WEBHOOK_URL'], json={'text': msg})
     app.logger.info('Posted 100% notification to slack for ' + name)
 
 
@@ -111,7 +117,8 @@ def sync_freshman(freshmen_list):
         if list_freshman.rit_username not in freshmen_in_db:
             # This is a new freshman so add them to the DB
             freshmen_in_db[list_freshman.rit_username] = Freshman(rit_username=list_freshman.rit_username,
-                                                                 name=list_freshman.name, onfloor=list_freshman.onfloor)
+                                                                  name=list_freshman.name,
+                                                                  onfloor=list_freshman.onfloor)
             db.session.add(freshmen_in_db[list_freshman.rit_username])
         else:
             # This freshman is already in the DB so just update them
@@ -133,9 +140,9 @@ def sync_freshman(freshmen_list):
         # pylint: disable=cell-var-from-loop
         current_fresh_sigs = set(map(lambda fresh_sig: fresh_sig.freshman_username, packet.fresh_signatures))
         for list_freshman in filter(lambda list_freshman: list_freshman.rit_username not in current_fresh_sigs and
-                                                        list_freshman.onfloor and
-                                                        list_freshman.rit_username != packet.freshman_username,
-                                   freshmen_list.values()):
+                                                          list_freshman.onfloor and
+                                                          list_freshman.rit_username != packet.freshman_username,
+                                    freshmen_list.values()):
             db.session.add(FreshSignature(packet=packet, freshman=freshmen_in_db[list_freshman.rit_username]))
 
     db.session.commit()
